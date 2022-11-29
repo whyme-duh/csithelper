b5b555e09656a75fec5ba27cdfc1af7dab72da57
whyme-duh <ritikshrestha94@gmail.com>
2022-11-29 17:50:12 +0545

    update

--- main/settings.py
+++ main/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = 'SECRET_KEY_SETTINGS'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
     
-ALLOWED_HOSTS = [ '*'] 
+ALLOWED_HOSTS = [  '192.168.1.67','*' ] 
 
 
 
